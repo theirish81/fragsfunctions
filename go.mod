8597156ccbc41d8036dc15d2d548e56bf092de8e
theirish81 <theirish81@gmail.com>
2025-12-28 07:21:12 +0100

    frags functions base library

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/theirish81/fragsfunctions
+
+go 1.24.0
+
+toolchain go1.24.8
+
+require github.com/theirish81/frags v0.12.0
---
+++ util.go
@@ -0,0 +1,48 @@
+package fragsfunctions
+
+import (
+	"errors"
+
+	"github.com/theirish81/frags"
+)
+
+func GetArg[T any](args map[string]any, key string) (*T, error) {
+	if v1, ok := args[key]; ok {
+		if v2, ok := v1.(T); ok {
+			return &v2, nil
+		}
+		return nil, errors.New(key + " argument is not of the expected type")
+	}
+	return nil, errors.New(key + " argument is required")
+}
+
+type BasicCollection struct {
+	name        string
+	description string
+	functions   frags.Functions
+}
+
+func NewBasicCollection(name, description string) BasicCollection {
+	return BasicCollection{
+		name:        name,
+		description: description,
+		functions:   make(frags.Functions),
+	}
+}
+
+func (bc *BasicCollection) AddFunction(f frags.Function) {
+	f.Collection = bc.name
+	bc.functions[f.Name] = f
+}
+
+func (bc *BasicCollection) Name() string {
+	return bc.name
+}
+
+func (bc *BasicCollection) Description() string {
+	return bc.description
+}
+
+func (bc *BasicCollection) AsFunctions() frags.Functions {
+	return bc.functions
+}
